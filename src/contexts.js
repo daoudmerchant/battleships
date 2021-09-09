385ec8362364a3adcf72eb7aa6f6980a0638b46e
Daoud Merchant <merchantdaoud@hotmail.com>
2021-09-09 10:33:50 +0200

    Update contexts

--- src/contexts.js
+++ src/contexts.js
@@ -1,6 +1,5 @@
 import { createContext } from "react";
 
-const VisibilityContext = createContext(),
-  GameOverContext = createContext();
+const VisibilityContext = createContext();
 
-export { VisibilityContext, GameOverContext };
+export { VisibilityContext };
